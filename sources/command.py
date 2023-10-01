fd4f29add5d7579c26375a579d93acae9522f4c3
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-01 23:45:39 +0200

    Add command wrappers for basic operations

--- sources/command.py
+++ sources/command.py
@@ -6,8 +6,21 @@ from pathlib import Path
 from subprocess import PIPE, Popen
 from typing import Union, List, Optional, Type
 
-from sources.exceptions import GitCommandException
+from sources.exceptions import GitCommandException, GitException, GitPushException, GitPullException, GitRmException, \
+    GitMvException, GitAddException, GitCloneException, GitInitException, GitShowException, GitConfigException, \
+    GitCheckoutException, GitForEachRefException
+from sources.options.add_options import AddCommandDefinitions
+from sources.options.checkout_options import CheckoutCommandDefinitions
+from sources.options.clone_options import CloneCommandDefinitions
+from sources.options.config_options import ConfigCommandDefinitions
+from sources.options.for_each_ref_options import ForEachRefCommandDefinitions
+from sources.options.init_options import InitCommandDefinitions
+from sources.options.mv_options import MvCommandDefinitions
 from sources.options.options import GitCommand, GitOption
+from sources.options.pull_options import PullCommandDefinitions
+from sources.options.push_options import PushCommandDefinitions
+from sources.options.rm_options import RmCommandDefinitions
+from sources.options.show_options import ShowCommandDefinitions
 
 
 class GitCommandRunner:
@@ -50,7 +63,7 @@ class GitCommandRunner:
         return command
 
     def execute(self, commands: List[Union[str, int, GitOption, None]],
-                definitions_class: Optional[Type[GitCommand]] = None, log_output: bool = False):
+                definitions_class: Optional[Type[GitCommand]] = None, log_output: bool = False) -> str:
         """
         Method executes git command with options provided as an input. It validates options based on the provided
         definition. If definitions class has not been provided, then no validation happens and standard list of commands
@@ -59,7 +72,7 @@ class GitCommandRunner:
         :param commands: List of options for the git command.
         :type commands: List[Union[str, int, GitOption, None]]
         :param definitions_class: Definition class that describes options for the git command, shall be derived from
-        GitCommand class.
+            GitCommand class.
         :type definitions_class: Optional[Type[GitCommand]]
         :param log_output: If this option is True, then stdout of the command will be logged,
         :type log_output: bool
@@ -83,3 +96,169 @@ class GitCommandRunner:
             if process.returncode != 0:
                 raise GitCommandException(stderr)
             return stdout
+
+    def __execute_git_command(self, options: List[GitOption], definition_class: Type[GitCommand],
+                              exception_class: Type[GitCommandException], log_output: bool = False):
+        """
+        Execute git command that takes GitOption, it shall has command definitions class, and exception class.
+        Optionally method can log stdout in the runtime, if the 'log_output' was set as True. As result of the
+        successful execution stdout will be returned.
+
+        :param options: List of git options for the command.
+        :type options: List[GitOption]
+        :param definition_class: Class that describes git command and its options.
+        :type definition_class: Type[GitCommand]
+        :param exception_class: Exception that will be raised if git command failed (returned non-zero exit code).
+        :type exception_class: Type[GitCommandException]
+        :param log_output: Select whether it shall log stdout in the runtime.
+        :type log_output: bool
+        :return: Stdout returned by the command.
+        """
+        try:
+            return self.execute(commands=options, definitions_class=definition_class, log_output=log_output)
+        except GitException as exception:
+            raise exception_class(exception.args[0]) from None
+
+    def init(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git init' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git init' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git init' command.
+        """
+        return self.__execute_git_command(list(options), InitCommandDefinitions, GitInitException, log_output)
+
+    def clone(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git clone' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git clone' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git clone' command.
+        """
+        return self.__execute_git_command(list(options), CloneCommandDefinitions, GitCloneException, log_output)
+
+    def add(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git add' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git add' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git add' command.
+        """
+        return self.__execute_git_command(list(options), AddCommandDefinitions, GitAddException, log_output)
+
+    def mv(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git mv' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git mv' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git mv' command.
+        """
+        return self.__execute_git_command(list(options), MvCommandDefinitions, GitMvException, log_output)
+
+    def rm(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git rm' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git rm' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git rm' command.
+        """
+        return self.__execute_git_command(list(options), RmCommandDefinitions, GitRmException, log_output)
+
+    def pull(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git pull' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git pull' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git pull' command.
+        """
+        return self.__execute_git_command(list(options), PullCommandDefinitions, GitPullException, log_output)
+
+    def push(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git push' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git push' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git push' command.
+        """
+        return self.__execute_git_command(list(options), PushCommandDefinitions, GitPushException, log_output)
+
+    def show(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git show' command with provided options and return stdout of the command. Optionally it can log stdout
+        in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git show' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git show' command.
+        """
+        return self.__execute_git_command(list(options), ShowCommandDefinitions, GitShowException, log_output)
+
+    def config(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git config' command with provided options and return stdout of the command. Optionally it can log
+        stdout in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git config' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git config' command.
+        """
+        return self.__execute_git_command(list(options), ConfigCommandDefinitions, GitConfigException, log_output)
+
+    def checkout(self, *options: GitOption, log_output: bool = False) -> str:
+        """
+        Execute 'git checkout' command with provided options and return stdout of the command. Optionally it can log
+        stdout in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git checkout' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git checkout' command.
+        """
+        return self.__execute_git_command(list(options), CheckoutCommandDefinitions, GitCheckoutException, log_output)
+
+    def for_each_ref(self, *options: GitOption, log_output: bool = False):
+        """
+        Execute 'git for-each-ref' command with provided options and return stdout of the command. Optionally it can log
+        stdout in the runtime, if 'log_output' option has been set as True.
+
+        :param options: Options for the 'git for-each-ref' command.
+        :type options: Tuple[GitOption]
+        :param log_output: Set as True, if it shall log stdout of the command in the runtime, otherwise False.
+        :type log_output: bool
+        :return: Stdout returned by 'git for-each-ref' command.
+        """
+        return self.__execute_git_command(list(options), ForEachRefCommandDefinitions, GitForEachRefException,
+                                          log_output)
