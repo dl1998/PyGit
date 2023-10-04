de35d090f6954fd3d29cf43e569278c7173b2ea2
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-04 22:09:24 +0200

    Add docstrings and typing to PathsMapping

--- sources/utils/path_util.py
+++ sources/utils/path_util.py
@@ -2,7 +2,7 @@
 Module contains class for manipulations with a path.
 """
 from pathlib import Path
-from typing import Union
+from typing import Union, NoReturn
 
 
 class PathUtil:
@@ -37,6 +37,9 @@ class PathUtil:
 
 
 class PathsMapping:
+    """
+    Class for paths mapping, maps source path with destination path.
+    """
     DELIMITER: str = ':'
 
     __source: Path
@@ -51,7 +54,17 @@ class PathsMapping:
         self.__destination = destination
 
     @classmethod
-    def create_from_text(cls, mapping: str, root_path: Union[str, Path] = None):
+    def create_from_text(cls, mapping: str, root_path: Union[str, Path] = None) -> 'PathsMapping':
+        """
+        Create a new class instance from the string mapping in the format "source:destination" that is relative to the
+        repository root. Optionally repository root can be provided, otherwise current directory considered as a root.
+
+        :param mapping: Source to destination mapping, format: "source:destination".
+        :type mapping: str
+        :param root_path: Path to the root in which files are located.
+        :type root_path: Union[str, Path]
+        :return: A new class instance.
+        """
         if root_path is None:
             root_path = Path()
         source, destination = mapping.strip().split(cls.DELIMITER)
@@ -61,6 +74,13 @@ class PathsMapping:
         return instance
 
     def __normalize_path(self, path: Union[str, Path]) -> Path:
+        """
+        Normalize the path, returns absolute path converted from string to Path, that always starts from the root path.
+
+        :param path: Path that will be normalized.
+        :type path: Union[str, Path]
+        :return: Normalized path.
+        """
         if isinstance(path, str) and str(path).startswith(str(self.__root_path)):
             normalized_path = Path(path)
         elif isinstance(path, str):
@@ -71,14 +91,23 @@ class PathsMapping:
 
     @property
     def source(self) -> Path:
+        """
+        Source path of the mapping.
+        """
         return self.__normalize_path(self.__source)
 
     @property
     def destination(self) -> Path:
+        """
+        Destination path of the mapping.
+        """
         return self.__normalize_path(self.__destination)
 
     @property
     def root_path(self) -> Path:
+        """
+        Root path of the repository, where the files are located.
+        """
         return self.__root_path
 
     @root_path.setter
