b20bdb4563a7ff2aca89ab2081353d25eb08bc05
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-28 23:01:35 +0200

    Add tests documentation for 'git config' options

--- tests/unit_tests/pygit/options/test_config_options.py
+++ tests/unit_tests/pygit/options/test_config_options.py
@@ -1,10 +1,22 @@
+"""
+Module contains unit tests for 'git config' options definition class.
+"""
 from pygit.options.testing_utils import CommandDefinitionsTestingUtil
 from sources.options.config_options import ConfigCommandDefinitions
 
 
 class TestConfigCommandDefinitions:
+    """
+    Class contains unit tests for 'ConfigCommandDefinitions' class.
+    """
     def test_command_positive(self):
+        """
+        Method tests that command in the 'ConfigCommandDefinitions' class has been properly set.
+        """
         CommandDefinitionsTestingUtil.test_command_positive(ConfigCommandDefinitions, 'config')
 
     def test_definitions_positive(self):
+        """
+        Method tests that definitions have been created for all options from the 'ConfigCommandDefinitions' class.
+        """
         CommandDefinitionsTestingUtil.test_definitions_positive(ConfigCommandDefinitions)
