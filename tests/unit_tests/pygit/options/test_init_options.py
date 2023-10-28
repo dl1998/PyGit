afb12204447bb8ee2b95a9234435dec91dc4ce45
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-28 23:07:19 +0200

    Add tests documentation for 'git init' options

--- tests/unit_tests/pygit/options/test_init_options.py
+++ tests/unit_tests/pygit/options/test_init_options.py
@@ -1,10 +1,22 @@
+"""
+Module contains unit tests for 'git init' options definition class.
+"""
 from pygit.options.testing_utils import CommandDefinitionsTestingUtil
 from sources.options.init_options import InitCommandDefinitions
 
 
 class TestInitCommandDefinitions:
+    """
+    Class contains unit tests for 'InitCommandDefinitions' class.
+    """
     def test_command_positive(self):
+        """
+        Method tests that command in the 'InitCommandDefinitions' class has been properly set.
+        """
         CommandDefinitionsTestingUtil.test_command_positive(InitCommandDefinitions, 'init')
 
     def test_definitions_positive(self):
+        """
+        Method tests that definitions have been created for all options from the 'InitCommandDefinitions' class.
+        """
         CommandDefinitionsTestingUtil.test_definitions_positive(InitCommandDefinitions)
