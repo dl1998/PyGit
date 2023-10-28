00bf5d2c5c3bc12c01561e5cee8e38a3b6a36144
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-28 23:13:13 +0200

    Add tests documentation for 'git show' options

--- tests/unit_tests/pygit/options/test_show_options.py
+++ tests/unit_tests/pygit/options/test_show_options.py
@@ -1,10 +1,22 @@
+"""
+Module contains unit tests for 'git show' options definition class.
+"""
 from pygit.options.testing_utils import CommandDefinitionsTestingUtil
 from sources.options.show_options import ShowCommandDefinitions
 
 
 class TestShowCommandDefinitions:
+    """
+    Class contains unit tests for 'ShowCommandDefinitions' class.
+    """
     def test_command_positive(self):
+        """
+        Method tests that command in the 'ShowCommandDefinitions' class has been properly set.
+        """
         CommandDefinitionsTestingUtil.test_command_positive(ShowCommandDefinitions, 'show')
 
     def test_definitions_positive(self):
+        """
+        Method tests that definitions have been created for all options from the 'ShowCommandDefinitions' class.
+        """
         CommandDefinitionsTestingUtil.test_definitions_positive(ShowCommandDefinitions)
