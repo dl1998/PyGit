1ab87baf5412de853be1af1eb2210ac7f5d4fab9
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-28 23:10:45 +0200

    Add tests documentation for 'git push' options

--- tests/unit_tests/pygit/options/test_push_options.py
+++ tests/unit_tests/pygit/options/test_push_options.py
@@ -1,10 +1,22 @@
+"""
+Module contains unit tests for 'git push' options definition class.
+"""
 from pygit.options.testing_utils import CommandDefinitionsTestingUtil
 from sources.options.push_options import PushCommandDefinitions
 
 
 class TestPushCommandDefinitions:
+    """
+    Class contains unit tests for 'PushCommandDefinitions' class.
+    """
     def test_command_positive(self):
+        """
+        Method tests that command in the 'PushCommandDefinitions' class has been properly set.
+        """
         CommandDefinitionsTestingUtil.test_command_positive(PushCommandDefinitions, 'push')
 
     def test_definitions_positive(self):
+        """
+        Method tests that definitions have been created for all options from the 'PushCommandDefinitions' class.
+        """
         CommandDefinitionsTestingUtil.test_definitions_positive(PushCommandDefinitions)
