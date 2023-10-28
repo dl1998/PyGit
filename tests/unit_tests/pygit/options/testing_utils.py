47316fee95b741a07d705bc1b472d8325bb4b590
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-10-28 23:17:32 +0200

    Add tests documentation for options utils class

--- tests/unit_tests/pygit/options/testing_utils.py
+++ tests/unit_tests/pygit/options/testing_utils.py
@@ -1,16 +1,28 @@
+"""
+Module contains testing utilities for options definitions classes.
+"""
 from typing import Type
 
 from sources.options.options import GitCommand
 
 
 class CommandDefinitionsTestingUtil:
+    """
+    Class contains unit tests for command definitions class.
+    """
     @staticmethod
     def test_command_positive(class_type: Type[GitCommand], expected_name: str):
+        """
+        Method tests that command in the command definitions class has been properly set.
+        """
         command = class_type()
         assert command.command_name == expected_name
 
     @staticmethod
     def test_definitions_positive(class_type: Type[GitCommand]):
+        """
+        Method tests that definitions have been created for all options from the command definitions class.
+        """
         command = class_type()
         missing_options = []
         for option in command.Options:
