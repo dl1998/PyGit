cf40b5273a960af8fc3ddeec80ec2805d73db960
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-12-26 20:44:19 +0100

    Add tests documentation for the tests for options module

--- tests/unit_tests/pygit/options/test_options.py
+++ tests/unit_tests/pygit/options/test_options.py
@@ -1,3 +1,6 @@
+"""
+Module contains unit tests for base options classes of git.
+"""
 from typing import Any, Type
 
 import pytest
@@ -9,7 +12,13 @@ from sources.options.options import GitOptionNameAliases, GitOptionNameAlias, Gi
 
 
 class DemoCommand(GitCommand):
+    """
+    Class inherits 'GitCommand' class and defines 'dummy' git command for testing purposes.
+    """
     class Options(CommandOptions):
+        """
+        Class inherits 'CommandOptions' class and defines 'dummy' options for the 'dummy' git command.
+        """
         OPTION = GitOptionNameAliases(aliases=[
             GitOptionNameAlias(name='option', short_option=False),
             GitOptionNameAlias(name='o', short_option=True),
@@ -36,9 +45,23 @@ class DemoCommand(GitCommand):
 
 
 class TestGitOptionNameAliases:
+    """
+    Class contains unit tests for 'GitOptionNameAliases' class.
+    """
     @pytest.mark.parametrize('name,short_option,length', [('option', False, 1), ('o', True, 1)],
                              ids=('Get long alias', 'Get short alias'))
     def test_get_aliases_positive(self, name: str, short_option: bool, length: int):
+        """
+        Method tests that 'get_aliases' method from 'GitOptionNameAliases' class returns correct values for short and
+        long aliases.
+
+        :param name: The name of the option.
+        :type name: str
+        :param short_option: Whether the tested option is short or long.
+        :type short_option: bool
+        :param length: The number fo the available aliases for this option.
+        :type length: int
+        """
         alias = GitOptionNameAlias(name=name, short_option=short_option)
         option_aliases = GitOptionNameAliases(aliases=[
             alias,
@@ -49,6 +72,10 @@ class TestGitOptionNameAliases:
 
     # noinspection PyTypeChecker
     def test_get_aliases_negative(self):
+        """
+        Method tests that 'get_aliases' method from 'GitOptionNameAliases' class throws an exception when an invalid
+        alias is on the list.
+        """
         alias = 'option'
         try:
             option_aliases = GitOptionNameAliases(aliases=[
@@ -63,6 +90,15 @@ class TestGitOptionNameAliases:
     @pytest.mark.parametrize('name,short_option', [('option', False), ('o', True)],
                              ids=("Without short options", 'With short options'))
     def test_has_short_aliases_positive(self, name: str, short_option: bool):
+        """
+        Method tests that 'has_short_aliases' method from 'GitOptionNameAliases' class returns True, if there is at
+        least one short alias for this option and otherwise returns False.
+
+        :param name: The name of the alias.
+        :type name: str
+        :param short_option: Whether this alias is short or not.
+        :type short_option: bool
+        """
         alias = GitOptionNameAlias(name=name, short_option=short_option)
         option_aliases = GitOptionNameAliases(aliases=[
             alias,
@@ -71,8 +107,17 @@ class TestGitOptionNameAliases:
         assert has_short_aliases == short_option
 
     @pytest.mark.parametrize('name,short_option', [('option', False), ('o', True)],
-                             ids=("Without short options", 'With short options'))
+                             ids=("With long options", 'Without long options'))
     def test_has_long_aliases_positive(self, name: str, short_option: bool):
+        """
+        Method tests that 'has_long_aliases' method from 'GitOptionNameAliases' class returns True, if there is at
+        least one long alias for this option and otherwise returns False.
+
+        :param name: The name of the alias.
+        :type name: str
+        :param short_option: Whether this alias is short or not.
+        :type short_option: bool
+        """
         alias = GitOptionNameAlias(name=name, short_option=short_option)
         option_aliases = GitOptionNameAliases(aliases=[
             alias,
@@ -83,6 +128,15 @@ class TestGitOptionNameAliases:
     @pytest.mark.parametrize('name,exists', [('option', True), ('o', True), ('another_option', False)],
                              ids=("Long option exists", 'Short option exists', 'Missing option'))
     def test_exists_positive(self, name: str, exists: bool):
+        """
+        Method tests that 'exists' method from 'GitOptionNameAliases' class returns True, if there is an alias with
+        provided name.
+
+        :param name: The name of the alias.
+        :type name: str
+        :param exists: Whether this alias exists or not.
+        :type exists: bool
+        """
         option_aliases = GitOptionNameAliases(aliases=[
             GitOptionNameAlias(name='option', short_option=False),
             GitOptionNameAlias(name='o', short_option=True),
@@ -91,6 +145,9 @@ class TestGitOptionNameAliases:
         assert option_exists == exists
 
     def test_get_names_positive(self):
+        """
+        Method tests that 'get_names' method from 'GitOptionNameAliases' class returns list of aliases for the option.
+        """
         option_aliases = GitOptionNameAliases(aliases=[
             GitOptionNameAlias(name='option', short_option=False),
             GitOptionNameAlias(name='o', short_option=True),
@@ -98,51 +155,86 @@ class TestGitOptionNameAliases:
         names = option_aliases.get_names()
         assert names == ['option', 'o']
 
-    class TestGitOptionDefinition:
-        @pytest.mark.parametrize('name,value,expected', [
-            ('option', True, True), ('option', 'value', True), ('o', True, True), ('o', 123, False),
-            ('another_option', 'value', False)
-        ], ids=('Correct long boolean option', 'Correct long string option', 'Correct short boolean option',
-                'Incorrect short integer option', 'Incorrect another string option'))
-        def test_compare_with_option_positive(self, name: str, value: Any, expected: bool):
-            name_aliases = GitOptionNameAliases(aliases=[
-                GitOptionNameAlias(name='option', short_option=False),
-                GitOptionNameAlias(name='o', short_option=True),
-            ])
-            definition = GitOptionDefinition(name_aliases=name_aliases, type=(str, bool))
-            option = GitOption(name=name, value=value)
-            comparison_result = definition.compare_with_option(option)
-            assert comparison_result == expected
 
-    class TestGitCommand:
-        @pytest.mark.parametrize('name,value,expected', [
-            ('option', True, True), ('option', 'value', True), ('o', True, True), ('o', 123, False),
-            ('another_option', 'value', False)
-        ], ids=("Definition exists for long boolean option", "Definition exists for long string option",
-                "Definition exists for short boolean option", "Definition doesn't exist for short integer option",
-                "Definition doesn't exist for another string option"))
-        def test_get_definition_positive(self, name: str, value: Any, expected: bool):
-            option = GitOption(name=name, value=value)
-            command = DemoCommand()
-            definition = command.get_definition(option)
-            if expected:
-                assert definition is not None
-            else:
-                assert definition is None
+class TestGitOptionDefinition:
+    """
+    Class contains unit tests for 'GitOptionDefinition' class.
+    """
+    @pytest.mark.parametrize('name,value,expected', [
+        ('option', True, True), ('option', 'value', True), ('o', True, True), ('o', 123, False),
+        ('another_option', 'value', False)
+    ], ids=('Correct long boolean option', 'Correct long string option', 'Correct short boolean option',
+            'Incorrect short integer option', 'Incorrect another string option'))
+    def test_compare_with_option_positive(self, name: str, value: Any, expected: bool):
+        """
+        Method tests that 'compare_with_option' method from class 'GitOptionDefinition' returns True if 'GitOption' is
+        corresponding to 'GitOptionDefinition', otherwise returns False.
+
+        :param name: The name of the option.
+        :type name: str
+        :param value: The value of the option.
+        :type value: Any
+        :param expected: The expected value that shall be returned by 'compare_with_option' method.
+        :type expected: bool
+        """
+        name_aliases = GitOptionNameAliases(aliases=[
+            GitOptionNameAlias(name='option', short_option=False),
+            GitOptionNameAlias(name='o', short_option=True),
+        ])
+        definition = GitOptionDefinition(name_aliases=name_aliases, type=(str, bool))
+        option = GitOption(name=name, value=value)
+        comparison_result = definition.compare_with_option(option)
+        assert comparison_result == expected
+
+
+class TestGitCommand:
+    """
+    Class contains unit tests for 'GitCommand' class.
+    """
+    @pytest.mark.parametrize("name,value,has_definition", [
+        ('option', True, True), ('option', 'value', True), ('o', True, True), ('o', 123, False),
+        ('another_option', 'value', False)
+    ], ids=("Definition exists for long boolean option", "Definition exists for long string option",
+            "Definition exists for short boolean option", "Definition doesn't exist for short integer option",
+            "Definition doesn't exist for long string option"))
+    def test_get_definition_positive(self, name: str, value: Any, has_definition: bool):
+        """
+        Method tests that 'get_definition' method from 'GitCommand' class returns definition for the given 'GitOption'
+        if it exists, otherwise returns None.
+
+        :param name: The name of the option.
+        :type name: str
+        :param value: The value of the option.
+        :type value: Any
+        :param has_definition: If expected is True, then non None value is expected to be returned by 'get_definition'
+            method, otherwise None is expected.
+        :type has_definition: bool
+        """
+        option = GitOption(name=name, value=value)
+        command = DemoCommand()
+        definition = command.get_definition(option)
+        if has_definition:
+            assert definition is not None
+        else:
+            assert definition is None
 
     def test_validate_positive(self):
+        """
+        Method tests that 'validate' method from 'GitCommand' class validates the options for the 'GitCommand'. It shall
+        not raise an exception for the correct options.
+        """
         options = [
             DemoCommand.Options.SHORT_OPTION.create_option(True),
             DemoCommand.Options.LONG_OPTION.create_option('first'),
             DemoCommand.Options.POSITIONAL_OPTION.create_option('value'),
         ]
         command = DemoCommand()
-        fired = False
+        raise_exception = False
         try:
             command.validate(options)
         except GitException:
-            fired = True
-        assert not fired
+            raise_exception = True
+        assert not raise_exception
 
     @pytest.mark.parametrize('name,value,expected_exception', [
         ('missing_option', True, GitMissingDefinitionException),
@@ -150,16 +242,38 @@ class TestGitOptionNameAliases:
         ('option', True, GitMissingRequiredOptionsException),
     ], ids=("Missing option exception", "Incorrect option value exception", "Missing required option exception"))
     def test_validate_negative(self, name: str, value: Any, expected_exception: Type[GitException]):
+        """
+        Method tests that 'validate' method from 'GitCommand' class validates the options for the 'GitCommand'. It shall
+        raise specified exceptions for the incorrect options.
+
+        Tests the following exceptions:
+
+        - GitMissingDefinitionException: When option is not on the list of defined options for the 'GitCommand', it also
+        could be that option has different type than defined.
+        - GitIncorrectOptionValueException: When the value for the option is not the choices list.
+        - GitMissingRequiredOptionsException: When option that is required is not present.
+
+        :param name: The name of the option to validate.
+        :type name: str
+        :param value: The value for the option.
+        :type value: Any
+        :param expected_exception: The exception type that shall be thrown for this option.
+        :type expected_exception: Type[GitException]
+        """
         option = GitOption(name=name, value=value)
         command = DemoCommand()
-        fired = False
+        raise_exception = False
         try:
             command.validate(option)
         except expected_exception:
-            fired = True
-        assert fired
+            raise_exception = True
+        assert raise_exception
 
     def test_transform_to_command_positive(self):
+        """
+        Method tests that 'transform_to_command' method from 'GitOption' class is able to correctly transform
+        'GitOption' list to raw command arguments.
+        """
         options = [
             DemoCommand.Options.SHORT_OPTION.create_option(True),
             DemoCommand.Options.LONG_OPTION.create_option('first'),
